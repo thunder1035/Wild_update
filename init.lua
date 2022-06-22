380535c7d2eaa5db1a4e3f6357794fab7c84c978
Thunder1035 <96511025+thunder1035@users.noreply.github.com>
2022-06-22 11:14:10 +0530

    Delete init.lua

--- init.lua
+++
@@ -1,8 +0,0 @@
-local modpath = minetest.get_modpath("wild_update")
-
--- Load files
-
-dofile(modpath .. "/blocks.lua")
-dofile(modpath .. "/entity.lua")
-dofile(modpath .. "/test.lua")
---dofile(modpath .. "/item.lua")--
