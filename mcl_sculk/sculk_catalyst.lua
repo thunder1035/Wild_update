2dad220a46c20a51b55f41ea44bc1fa621d71d00
Thunder1035 <96511025+thunder1035@users.noreply.github.com>
2023-04-07 16:21:52 +0530

    Add files via upload

---
+++ mcl_sculk/sculk_catalyst.lua
@@ -0,0 +1,15 @@
+--not in use until the full set is ready--
+
+--sculk stuff--
+
+--1)sculk block--
+--2)sculk catalyst--
+--3)sculk sensors--
+--4)sculk shrieker--
+--5)sculk vein--
+
+local modpath = minetest.get_modpath("mcl_sculk")
+
+-- Load files
+
+dofile(modpath .. "/.lua")
